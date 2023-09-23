e86fe1ce69c418e5238f654c0f180c5d82079ad0
Thryanii <984524888@qq.com>
2023-09-23 11:54:57 +0800

    change app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.3.0"
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
